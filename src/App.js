634565258f9ecccd3f46397b55415bf783130c01
Saurav Sah <sauravsah@Sauravs-MacBook-Pro.local>
2022-01-19 13:42:02 -0600

    title added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Header from './components/Header'
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello From React!</h1>
-      <h2>Hello Second</h2>
+    <div className="container">
+      <Header />
+      
     </div>
   );
 }

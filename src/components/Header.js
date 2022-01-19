e189b43a05be9870a734b3b55775ab1c2befc9fe
Saurav Sah <sauravsah@Sauravs-MacBook-Pro.local>
2022-01-19 14:44:15 -0600

    button component added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({title}) => {
-  return <header>
+  return (
+    <header className='header'>
       <h1 >{title}</h1>
-  </header>;
+      <Button color = 'green' text = 'Hello'/>
+      <Button color = 'red' text = 'World'/>
+    </header>
+  )
 };
 
 Header.defaultProps = {

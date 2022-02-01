d1136eabf5fce8e703ed9d6781cf7f821f024c33
Saurav Sah <sauravsah@Sauravs-MacBook-Pro.local>
2022-02-01 03:29:53 -0600

    delete function added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
+import { useState } from "react"
 import Header from './components/Header'
 import Tasks  from './components/Tasks';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: true,
+    },
+    {
+      id:2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 2:30 pm',
+      reminder: true,
+    },
+    {
+      id:3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: false,
+    },
+  ])
 
+  // Delete the task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task)=> task.id !== id))
+  }
   return (
     <div className="container">
       <Header />
-      <Tasks />
-      
+      {tasks.length > 0 ? (<Tasks tasks = {tasks} 
+      onDelete = {deleteTask}/>) : ('No tasks to Show')
+      } 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

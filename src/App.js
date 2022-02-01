21c2cc0f2d593ddad1bdf21b7987d02621b5767f
Saurav Sah <sauravsah@Sauravs-MacBook-Pro.local>
2022-02-01 16:59:00 -0600

    double click added

--- src/App.js
+++ src/App.js
@@ -29,11 +29,16 @@ const App = () => {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task)=> task.id !== id))
   }
+
+  //Toggle Reminder
+  const toggleReminder = (id) =>{
+    console.log(id)
+  }
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (<Tasks tasks = {tasks} 
-      onDelete = {deleteTask}/>) : ('No tasks to Show')
+      onDelete = {deleteTask} onToggle = {toggleReminder} />) : ('No tasks to Show')
       } 
     </div>
   )
